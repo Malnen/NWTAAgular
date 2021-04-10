bc800ef7a40f674fbc97514237fa0f1fc28ddd4b
Malnen <46851853+Malnen@users.noreply.github.com>
2021-04-10 17:10:18 +0200

    Revert "first-commit"
    This reverts commit 4be9d3771ced36fe3c4eb1eec7fa3b5dcd52476a.

--- src/shop/category/category.ts
+++
@@ -1,6 +0,0 @@
-export class Category {
-  id: number;
-  name: string;
-  description: string;
-  icon: string;         // NIE MA POTRZEBY DODAWANIA TEGO W BAZIE
-}
--- src/shop/item/Item.ts
+++
@@ -1,7 +0,0 @@
-export class Item {
-    id: number;
-    name: string;
-    price: number;
-    description: string;
-    categoryId: number;
-  }
