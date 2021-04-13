eb188c5ec8e40f6a15195f5b48b704cc3c06207d
jkrolik1 <56318896+jkrolik1@users.noreply.github.com>
2021-04-13 18:52:23 +0200

    Kolejne testy

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -21,6 +21,80 @@ describe('workspace-project App', () => {
     expect(await page.getUserName().getText()).toEqual('admin');
   });
 
+  it('should read category', async () => {
+    await page.navigateToAboutMe();
+    expect(await page.getCategoryText().getText()).toEqual('O naszym sklepie');
+    await page.sleep(3000);
+  });
+
+  it('check if correct number of products were added to chart', async () => {
+    let i: number;
+
+
+    // login out
+    await page.navigateTo();
+    await page.getLoginScreenButton().click();
+
+
+    // login
+    await page.navigateTo();
+    await page.getLoginScreenButton().click();
+    await page.getLoginInput().sendKeys("admin");
+    await page.getPasswordInput().sendKeys("admin");
+    await page.getLoginButton().click();
+
+    await page.sleep(3000);
+
+
+    // remove old addition from chart
+    await page.navigateToOrder();
+
+    await page.sleep(3000);
+
+    var parents = await page.getProducts();
+
+    parents.find('xxx');
+  
+/*
+    await page.sleep(3000);
+
+
+    for (i=0; i < size; i++)
+      if (expect(await page.getProductsNameInChart().getText()).toEqual('Tetra Pleco Veggie 250ml'))
+        await page.getChartButton(i).click();
+    
+
+    // add specify quantity od product
+    await page.navigateToCategory1();
+    await page.openProduct();
+    await page.getAmountOfProductsInput().sendKeys("5");
+    await page.addProductToChart();
+
+    await page.navigateToOrder();
+
+    for (i=0; i < size; i++)
+      if (expect(await page.getProductsNameInChart().getText()).toEqual('Tetra Pleco Veggie 250ml'))
+        expect(await page.getCategoryText().getText()).toEqual('5');
+
+
+    */
+  });
+
+  /*it('should login, then add and delete product from chart', async () => {
+    await page.navigateTo();
+    await page.getLoginScreenButton().click();
+    await page.getLoginInput().sendKeys("admin");
+    await page.getPasswordInput().sendKeys("admin");
+    await page.getLoginButton().click();
+
+    await page.sleep(3000);
+
+    await page.navigateToCategory1();
+    await page.openProduct();
+
+
+  });*/
+
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
     const logs = await browser.manage().logs().get(logging.Type.BROWSER);
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -23,4 +23,39 @@ export class AppPage {
   getUserName() {
     return element(by.css('body > app-root > div.sidebar > div.login_feature > h4'))
   }
+
+  navigateToAboutMe() {
+    return browser.get('/info');
+  }
+  getCategoryText() {
+    return element(by.css('body > app-root > div.content > app-info > div.choosenCategory'));
+  }
+
+  navigateToCategory1() {
+    return browser.get('/category/1');
+  }
+  openProduct() {
+    return element(by.css('body > app-root > div.content > app-shop > div.shop > div##mainSide > a:nth-child(2)'))
+  }
+  addProductToChart() {
+    return element(by.css('body > app-root > div.content > app-item > div.item-mobile > div.buy > input[type=number]:nth-child(2)'))
+  }
+  getAmountOfProductsInput() {
+    return element(by.css('body > app-root > div.content > app-item > div.item-mobile > div.buy > input[type=number]:nth-child(2)'))
+  }
+  navigateToOrder() {
+    return browser.get('/order');
+  }
+  getProducts(){
+    return element.all(by.css('body > app-root > div.content > app-order > div > div > a'));
+  }
+  getProductsNameInChart(){
+    return element(by.css('body > app-root > div.content > app-order > div > div > a'));
+  }
+  getChartButton(i) {
+    return element(by.css('body > app-root > div.content > app-login > div > div > div:nth-child(i) > button'))
+  }
+
+  
+
 }
